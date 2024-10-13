d4a159ca35c6244ace5bbb9b28d88f99eb76154e
Devansh Rai <devanshrai2003@gmail.com>
2024-10-14 01:04:17 +0530

    added origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { default: axios } = require("axios");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://aspire-task-management.onrender.com' }));
 app.use(express.json());
 
 app.use("/user", userRouter);
